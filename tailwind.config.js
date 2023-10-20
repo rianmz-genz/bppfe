5e59df9f6663091af6cbb2a7b6f9af0641fe8bef
rianmz-genz <adrianji1945@gmail.com>
2023-10-20 10:50:00 +0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'poppins': ['Poppins'],
-        'ysabeau': ['Ysabeau'],
-      }
-    },
-  },
-  plugins: [],
-}
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+   content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+      extend: {
+         fontFamily: {
+            poppins: ["Poppins"],
+            ysabeau: ["Ysabeau"],
+         },
+      },
+   },
+   plugins: [],
+});
