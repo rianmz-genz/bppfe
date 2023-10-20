cdf5c3975d960e1e09c7169f05e1109930100915
rianmz-genz <adrianji1945@gmail.com>
2023-10-20 23:07:29 +0700

    p

--- src/api/routes/Products.js
+++ src/api/routes/Products.js
@@ -2,3 +2,5 @@ import Url from "./Domain";
 
 export const urlGetAllProduct = `${Url}/products/all`;
 export const urlCreateProduct = `${Url}/products/create`;
+export const urlDeleteProduct = `${Url}/products/delete`;
+export const urlEditProduct = `${Url}/products/update`;
