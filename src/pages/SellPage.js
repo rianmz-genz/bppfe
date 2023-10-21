d76cb53c77945eaf158a5bf641aff2e03baaa362
rianmz-genz <adrianji1945@gmail.com>
2023-10-21 22:16:04 +0700

    pages

--- src/pages/SellPage.js
+++ src/pages/SellPage.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DefaultLayout from "../components/layout";
-import { BreadCrumbsSell } from "../components/sells";
+import { Sell } from "../components/sells";
+import GetAllProduct from "../api/integrations/Products/GetAll";
+import Chekout from "../components/checkout/Chekout";
 
 const SellPage = () => {
+   const [isLoading, setIsLoading] = useState(false);
+   const [isCheckout, setIsCheckout] = useState(false);
+   const [data, setData] = useState(false);
+   const [productSells, setProductSells] = useState([]);
+   useEffect(() => {
+      getAll();
+   }, []);
+   const getAll = () => {
+      setIsLoading(true);
+      GetAllProduct()
+         .then((res) => {
+            setIsLoading(false);
+            if (res) {
+               setData(res.data);
+            }
+         })
+         .catch((e) => {
+            console.log(e);
+         });
+   };
+
    return (
       <DefaultLayout>
-         <BreadCrumbsSell />
-         <h1 className="text-4xl font-ysabeau font-bold mb-4 my-2">
-            Penjualan
-         </h1>
+         {isCheckout ? (
+            <Chekout />
+         ) : (
+            <Sell
+               data={data}
+               getAll={getAll}
+               isLoading={isLoading}
+               productSells={productSells}
+               setProductSells={setProductSells}
+               onCheckout={() => setIsCheckout(true)}
+            />
+         )}
       </DefaultLayout>
    );
 };
