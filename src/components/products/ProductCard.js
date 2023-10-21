ad2e85e65a3b15d671a05e8ccdd597c05230ef87
rianmz-genz <adrianji1945@gmail.com>
2023-10-21 22:15:37 +0700

    product card

---
+++ src/components/products/ProductCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import DialogEditProduct from "./DialogEdit";
+import DialogDeleteProduct from "./DialogDelete";
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
+
+const ProductCard = ({
+   item,
+   getAll,
+   isSell = false,
+   qty,
+   increment,
+   decrement,
+}) => {
+   return (
+      <div className="w-48 flex flex-col justify-center items-start space-y-1">
+         <img
+            className="w-48 rounded-md h-48 object-cover"
+            src={`data:image/png;base64,${item?.image}`}
+            alt={item?.name}
+         />
+         <p className="text-2xl font-ysabeau font-bold">{item?.name}</p>
+         <p>Rp. {item?.price.toLocaleString("id-ID")}</p>
+         {isSell ? (
+            <div className="flex items-center gap-2">
+               <AiOutlineMinusCircle onClick={decrement} />
+               <p>{qty}</p>
+               <AiOutlinePlusCircle onClick={increment} />
+            </div>
+         ) : (
+            <div>
+               <DialogDeleteProduct id={item?.id} getAll={getAll} />
+               <DialogEditProduct
+                  name={item?.name}
+                  price={item?.price}
+                  id={item?.id}
+                  getAll={getAll}
+               />
+            </div>
+         )}
+      </div>
+   );
+};
+
+export default ProductCard;
