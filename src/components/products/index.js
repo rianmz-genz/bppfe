4326f9823f21ed26303ec35363e4caf7c757c28c
rianmz-genz <adrianji1945@gmail.com>
2023-10-21 22:15:31 +0700

    products

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -2,3 +2,4 @@ export { default as BreadCrumbsProduct } from "./BreadCrumbs";
 export { default as DialogCreateProduct } from "./DialogCreate";
 export { default as DialogDeleteProduct } from "./DialogDelete";
 export { default as DialogEditProduct } from "./DialogEdit";
+export { default as ProductCard } from "./ProductCard";
