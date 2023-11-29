d1c09b657229e86e7e0a7b485ecdf2d842de1c0e
rianmz-genz <adrianji1945@gmail.com>
2023-11-29 23:12:32 +0700

    history detail

--- src/components/history/detail/BreadCrumbs.js
+++ src/components/history/detail/BreadCrumbs.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const BreadCrumbsDetailHistory = () => {
    return (
       <div className="flex space-x-2">
          <p className="text-sm">EAA</p>
          <p className="text-sm text-black">/</p>
-         <p className="text-sm text-black">Penjualan</p>
+         <Link to={"/history"} className="text-sm text-black">
+            Penjualan
+         </Link>
          <p className="text-sm text-black">/</p>
          <p className="text-sm text-black/40">Riwayat</p>
       </div>
